927154e0284f7b4f10ec3f986b1415531f891424
Ronan Pardo Soares <ronan.soares@gmail.com>
2023-06-07 09:55:36 -0300

    Linting

--- test/test_main.py
+++ test/test_main.py
@@ -1,82 +1,108 @@
 """Esse arquivo testa o arquivo main.py"""
 
-import importlib  # for importing the main.py file
-import io  # for capturing the output
-import sys  # for restoring the stdout and removing the main module from the cache
-import unittest  # for creating the test case
-from unittest.mock import patch  # for mocking the input
+import importlib  # para importar o módulo main dinamicamente
+import io  # para capturar a saída do print
+import sys  # para restaurar o stdout padrão e remover o módulo main do cache
+import unittest  # para criar o caso de teste
+from unittest.mock import patch  # para simular a entrada do usuário
 
 
 class TestMain(unittest.TestCase):
     """Class for testing the main.py file"""
 
     def setUp(self):
-        """Sets up the test environment by removing the main module from the cache"""
-        super().setUp()
+        """
+        Inicializa o ambiente de teste removendo o módulo principal do cache.
+        Isso é necessário para ser capaz de importá-lo novamente em cada teste.
+        """
         sys.modules.pop("main", None)
 
     @patch("builtins.input", return_value="0")
     def test_tintas_0(self, _mock_input):
-        """Testa se o programa imprime "Serão necessárias 0 latas totalizando R$ 0"
-        quando o usuário digita 0"""
+        """
+        Testa se o programa imprime "Serão necessárias 0 latas totalizando R$ 0"
+        quando o usuário digita 0
+        """
         captured_output = io.StringIO()
         sys.stdout = captured_output
         importlib.import_module("main")
         sys.stdout = sys.__stdout__
-        self.assertIn("Serão necessárias 0 latas totalizando R$ 0",
-                      captured_output.getvalue().strip())
+        self.assertIn(
+            "Serão necessárias 0 latas totalizando R$ 0",
+            captured_output.getvalue().strip(),
+        )
 
     @patch("builtins.input", return_value="1")
     def test_tintas_1(self, _mock_input):
-        """Testa se o programa imprime "Serão necessárias 1 latas totalizando R$ 80"
-        quando o usuário digita 1"""
+        """
+        Testa se o programa imprime "Serão necessárias 1 latas totalizando R$ 80"
+        quando o usuário digita 1
+        """
         captured_output = io.StringIO()
         sys.stdout = captured_output
         importlib.import_module("main")
         sys.stdout = sys.__stdout__
-        self.assertIn("Serão necessárias 1 latas totalizando R$ 80",
-                      captured_output.getvalue().strip())
+        self.assertIn(
+            "Serão necessárias 1 latas totalizando R$ 80",
+            captured_output.getvalue().strip(),
+        )
 
     @patch("builtins.input", return_value="2")
     def test_tintas_2(self, _mock_input):
-        """Testa se o programa imprime "Serão necessárias 1 latas totalizando R$ 80"
-        quando o usuário digita 2"""
+        """
+        Testa se o programa imprime "Serão necessárias 1 latas totalizando R$ 80"
+        quando o usuário digita 2
+        """
         captured_output = io.StringIO()
         sys.stdout = captured_output
         importlib.import_module("main")
         sys.stdout = sys.__stdout__
-        self.assertIn("Serão necessárias 1 latas totalizando R$ 80",
-                      captured_output.getvalue().strip())
+        self.assertIn(
+            "Serão necessárias 1 latas totalizando R$ 80",
+            captured_output.getvalue().strip(),
+        )
 
     @patch("builtins.input", return_value="54")
     def test_tintas_54(self, _mock_input):
-        """Testa se o programa imprime "Serão necessárias 1 latas totalizando R$ 80"
-        quando o usuário digita 54"""
+        """
+        Testa se o programa imprime "Serão necessárias 1 latas totalizando R$ 80"
+        quando o usuário digita 54
+        """
         captured_output = io.StringIO()
         sys.stdout = captured_output
         importlib.import_module("main")
         sys.stdout = sys.__stdout__
-        self.assertIn("Serão necessárias 1 latas totalizando R$ 80",
-                      captured_output.getvalue().strip())
+        self.assertIn(
+            "Serão necessárias 1 latas totalizando R$ 80",
+            captured_output.getvalue().strip(),
+        )
 
     @patch("builtins.input", return_value="55")
     def test_tintas_55(self, _mock_input):
-        """Testa se o programa imprime "Serão necessárias 2 latas totalizando R$ 160"
-        quando o usuário digita 55"""
+        """
+        Testa se o programa imprime "Serão necessárias 2 latas totalizando R$ 160"
+        quando o usuário digita 55
+        """
         captured_output = io.StringIO()
         sys.stdout = captured_output
         importlib.import_module("main")
         sys.stdout = sys.__stdout__
-        self.assertIn("Serão necessárias 2 latas totalizando R$ 160",
-                      captured_output.getvalue().strip())
+        self.assertIn(
+            "Serão necessárias 2 latas totalizando R$ 160",
+            captured_output.getvalue().strip(),
+        )
 
     @patch("builtins.input", return_value="215")
     def test_tintas_215(self, _mock_input):
-        """Testa se o programa imprime "Serão necessárias 4 latas totalizando R$ 320"
-        quando o usuário digita 215"""
+        """
+        Testa se o programa imprime "Serão necessárias 4 latas totalizando R$ 320"
+        quando o usuário digita 215
+        """
         captured_output = io.StringIO()
         sys.stdout = captured_output
         importlib.import_module("main")
         sys.stdout = sys.__stdout__
-        self.assertIn("Serão necessárias 4 latas totalizando R$ 320",
-                      captured_output.getvalue().strip())
+        self.assertIn(
+            "Serão necessárias 4 latas totalizando R$ 320",
+            captured_output.getvalue().strip(),
+        )
